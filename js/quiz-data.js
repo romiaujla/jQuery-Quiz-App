cc935c5f23dd172840dae245d74f4e40c890699c
Raman Aujla <ramanaujla@Ramneets-MacBook-Air.local>
2019-05-27 11:59:27 -0400

    Stored quiz questions in quiz-data.js

---
+++ js/quiz-data.js
@@ -0,0 +1,115 @@
+const QUIZDATA = [
+    {
+        question : `Which element has the symbol "Cu" in the periodic table`,
+        options: [
+            "Carbon",
+            "Gold",
+            "Copper",
+            "Nickel"
+        ],
+        answer: "Copper",
+        answertext: `Copper is symbolized as "Cu". The Latin word "cuprum," meaning "copper," derives from the island of Cyprus, where it was often mined by the ancient Romans.
+        <br>Carbon is symbolized as "C"
+        <br>Gold is symbolized as "Au"
+        <br>Nickel is symbolized as "Ni"`
+    },
+    {
+        question : "The moons Charon and Styx belong to which dwarf planet?",
+        options : [
+            "Pluto",
+            "Jupiter",
+            "Venus",
+            "Mars"
+        ],
+        answer : "Pluto",
+        answertext : `Pluto's planetary status has been subject to debate in recent years, but scientists do agree about its moons. Pluto has five moons - Charon, Styx, Nyx, Hydra, and Kerberos - which are named for figures associated with Pluto/Hades in Greek mythology.`
+    },
+    {
+        question : "What is the term for a group of fish?",
+        options : [
+            "School",
+            "Herd",
+            "Flock",
+            "Gatherers"
+        ],
+        answer : "School",
+        answertext : `"Schooling" generally refers to when fish are swimming in a group, almost in a synchronized way. When fish gather with less coordination, it's called "shoaling" instead.`
+    },
+    {
+        question : `What is the term for the study of birds?`,
+        options : [
+            "Ichthyology",
+            "Apiology",
+            "Ornithology",
+            "Herpetology"
+        ],
+        answer : "Ornithology",
+        answertext : `Ornithology gets its name from the Greek word "ὄρνις/ornis," which means "bird." Records of formal bird-related studies date back as far as 1500 BCE.`
+    },
+    {
+        question : `What is the name of the chart that arranges elements into categories?`,
+        options: [
+            "Bloom's taxonomy",
+            "Periodic table",
+            "Maslow's hierarchy",
+            "Tonti Diagram"
+        ],
+        answer : "Periodic table",
+        answertext : `The periodic table shows the symbols and atomic numbers of the known chemical elements. It also divides the elements into categories such as noble gases and metals.`
+    }
+    {
+        question : `Octopuses and squid are classified as what type of animal?`,
+        options: [
+            "Fish",
+            "Arthopod",
+            "Crustacean",
+            "Cephalpopd"
+        ],
+        answer : "Cephalopod",
+        answertext: `Sometimes fishermen colloquially call this class of animal "inkfish." There are currently over 800 known species of cephalopod.`
+    },
+    {
+        question: `What is used to measure the magnitude of an earthquake?`,
+        options : [
+            "Scoville Scale",
+            "Anemoscope",
+            "Barometer",
+            "Richter Scale"
+        ],
+        answer : "Richter Scale",
+        answertext : `The Richter magnitude scale refers to a method of classifying the severity of earthquakes. Anything below a 4 is considered fairly minor, and a 7 or above is considered much more destructive.`
+    },
+    {
+        question : `What do you call the green pigments that give plants their green color?`,
+        options : [
+            "Chloropigments",
+            "Cyanophyll",
+            "Cyanopigments",
+            "Chlorophyll"
+        ],
+        answer : "Chloropyll",
+        answertext : `Photosynthesis, a vital process for plants, wouldn't be possible without chlorophyll. These green pigments help plants and algae absorb light.`
+    },
+    {
+        question : `What mnemonic device is used to help you remember the colors of the visible spectrum`,
+        options : [
+            "ROY G. BIV",
+            "FANBOYS",
+            "HOMES",
+            "PEMDAS"
+        ],
+        answer : `ROY G. BIV`,
+        answertext: `The part of the electromagnetic spectrum that can be seen by the human eye is called the visible spectrum. When light passes through a prism, it is dispersed into red, orange, yellow, green, blue, indigo, and violet, hence the mnemonic ROY G. BIV.`
+    },
+    {
+        question : `What is the outermost layer of the Earth?`,
+        options: [
+            "Outer core",
+            "Shell",
+            "Crust",
+            "Mantle"
+        ],
+        answer : "Crust",
+        answertext : "The Earth's crust makes up a very small portion of the planet's overall volume. The crust and the upper mantle make up a portion of Earth's structure called the lithosphere.",
+    }
+];
